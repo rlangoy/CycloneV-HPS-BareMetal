1080109e9859afda8114e704199b086fee732822
Rune Langøy <rune.langoy@gmail.com>
2020-11-11 14:20:12 +0100

    file update

--- CRC32/DE0-NANO-SoC/HW/ip/CRC32/avs_crc32_wraper_tb.sv
+++ CRC32/DE0-NANO-SoC/HW/ip/CRC32/avs_crc32_wraper_tb.sv
@@ -12,7 +12,7 @@ module test;
   
   
   // Instantiate design under test
-  avs_mm_wraper DUT(.clk(clk), .reset(reset),
+  avs_mm_crc32_wraper DUT(.clk(clk), .reset(reset),
                     .avs_address(address),                     
                     .avs_read(read), 
                     .avs_readdata(readdata),
@@ -25,29 +25,35 @@ module test;
       read  = 0;
       clk   = 0; 
       reset = 1;
+      write=0;
       address =8'b0;
       #1
       reset = 0; 
       #1
-      read  = 1;
-      
+      writedata= 8'h1;
+      write=1;
+    
+   	   
     end 
 
   
     always  
       #1  clk =  ! clk; 
-  	always
+   always
       #10  read=  ! read; 
+   always
+     #10  write=  ! write; 
+
   	always
-  	  #50  address= 8'h01;
+  	  #50  writedata= 8'h2;
   	always
   	  #80  address= 8'h02;
 
   
   
   initial  begin
-    $display("\t\ttime,\tclk,\treset,\tread\taddress\treaddatat"); 
-    $monitor("%d,\t%b,\t%b,\t%b,\t0x%h,\t0x%h",$time, clk,reset,read,address,readdata); 
+    $display("\t\ttime,\tclk,\treset,\tread\twrite\taddress\treaddatat"); 
+    $monitor("%d,\t%b,\t%b,\t%b,\t0x%h,\t0x%h,\t0x%h",$time, clk,reset,read,write,address,readdata); 
     end 
    
   initial 
