3dcea203cf50bf1ed10df877e56888ee72de5e1f
Rune Langøy <rune.langoy@gmail.com>
2020-12-01 09:56:02 +0100

    placed output  message in static string

--- Hello01/hello01.c
+++ Hello01/hello01.c
@@ -11,12 +11,12 @@
     https://www.intel.com/content/dam/www/programmable/us/en/pdfs/literature/hb/cyclone-v/cv_54001.pdf
 */
 
- 
+
 #define UART0_BASE 0xFFC02000   //Page  3082
 #define UART0_RBR_THR_DLL ((unsigned int *) (UART0_BASE + 0x00)) // Multi-function register. That holds the  transmit data 
 
 volatile unsigned int * const TxRxUART0 = UART0_RBR_THR_DLL;
-	 
+
 void print_uart1(const char *s) 
 {
     while(*s != '\0') 
@@ -25,9 +25,10 @@ void print_uart1(const char *s)
 	 s++; /* Next char */
     }
 }
-	 
-void c_entry() 
+static const char outPutString[] = "\r\nHello world!";
+
+void c_entry()
 {
-   print_uart1("\r\nHello world!");
+   print_uart1(outPutString);
    while(1) ; /*dont exit the program*/
 }
